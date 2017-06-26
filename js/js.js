9ffe5c3320a1bfef7c99462b6d7ebcdc18d21e73
Diogo Lopes <diogolopes2002@outlook.de>
2017-06-26 18:48:41 +0200

    Array test

--- js/js.js
+++ js/js.js
@@ -1,8 +1,8 @@
 function submit() {
   var userInput = document.getElementById('userinput');
   var list = document.getElementById('list');
-  list.innerHTML += '<li>' + userInput.value + '</li>'
-  userInput.focus()
+  list.innerHTML += '<li>' + userInput.value + '</li>';
+  userInput.focus();
   userInput.value=''
 }
 
@@ -13,6 +13,12 @@ function clearA() {
 }
 
 function addItems(items) {
+  var list = document.getElementById('list');
+  var arrayLength = items.length
+  for (i = 0; i < arrayLength; i++) {
+      list.innerHTML += '<li>' + items[i] + '</li>';
+  }
+
   // work here
   console.log(items);
 }
